822d888994784469439b868ba8c07d9630237e66
Yu-Jen Chen <chen.1571@osu.edu>
2023-03-08 21:35:08 -0800

    rewrite TIMS notebook

--- data_utils.py
+++ data_utils.py
@@ -2,9 +2,12 @@
 This data utility module provides functions to perform the following operations.
 
 Functions:
-- read_shp_to_gdf: read shapefile to
+- coordinate_to_geometry:
 - plot_route_map_flow: capitalizes the first letter of each word in the input string
 """
+
+import geopandas as gpd
+from shapely.geometry import Point
 
 
 def shp_to_gdf(shp_list):
@@ -19,28 +22,47 @@ def shp_to_gdf(shp_list):
     return gdf_results
 
 
+def coordinate_to_geometry(df, lon_col="lon", lat_col="lat", desired_crs="EPSG:4019"):
+    """
+    Convert coordiante to geometry with crs and output is gdf
+    :param df: pandas.DataFrame,
+    :param lon_col: str, longitude
+    :param lat_col: str, latitude
+    :param desired_crs: str, crs
+    :return: geopandas.GeoDataFrame, including geometry column
+    """
+    if "lon" not in df.columns or "lat" not in df.columns:
+        raise KeyError(
+            "The input dataframe must contain '{}' and '{}'.".format(lon_col, lat_col)
+        )
+    if "geometry" not in df.columns:
+        geometry = [Point(xy) for xy in zip(df[lon_col], df[lat_col])]
+        gdf = gpd.GeoDataFrame(df, crs=desired_crs, geometry=geometry)
+    else:
+        gdf = df.copy()
+    return gdf
+
+
 def plot_route_map_flow(dict_gdf: dict):
-    list_plots = []
-    for gdf_key, gdf_value in dict_gdf.items():
-        gdf_value.head(5)
-        # fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10, 10), layout="constrained")
-        # color_column = 'TOT_VOL'
-        # cmap = 'RdYlBu_r'
-        # gdf_value.plot(column=color_column, cmap=cmap, linewidth=5, ax=ax1)
-        # ctx.add_basemap(ax=ax1, zoom=13, crs='EPSG:4019', source=ctx.providers.Esri.WorldStreetMap)
+    pass
+    # fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(10, 10), layout="constrained")
+    # color_column = 'TOT_VOL'
+    # cmap = 'RdYlBu_r'
+    # gdf_value.plot(column=color_column, cmap=cmap, linewidth=5, ax=ax1)
+    # ctx.add_basemap(ax=ax1, zoom=13, crs='EPSG:4019', source=ctx.providers.Esri.WorldStreetMap)
 
-        # # set up xlabel, ylabel, and title for ax1
-        # ax1.set_xlabel('long')
-        # ax1.set_ylabel('lat')
-        # ax1.set_title('Map of SR74')
-        # # set up legend
-        # # vmin, vmax = SR74_BY2019_gdf[color_column].min(), SR74_BY2019_gdf[color_column].max()
-        # # sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-        # # cbar = fig.colorbar(sm)
+    # # set up xlabel, ylabel, and title for ax1
+    # ax1.set_xlabel('long')
+    # ax1.set_ylabel('lat')
+    # ax1.set_title('Map of SR74')
+    # # set up legend
+    # # vmin, vmax = SR74_BY2019_gdf[color_column].min(), SR74_BY2019_gdf[color_column].max()
+    # # sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+    # # cbar = fig.colorbar(sm)
 
-        # ax2.plot(SR74_BY2019_pm['PM'], SR74_BY2019_pm['TOT_VOL'])
+    # ax2.plot(SR74_BY2019_pm['PM'], SR74_BY2019_pm['TOT_VOL'])
 
-        # ax2.set_xlabel('Postmile')
-        # ax2.set_ylabel('Total flow')
-        # ax2.set_title('SR74 Total Flow BY2019')
-        # plt.show()
+    # ax2.set_xlabel('Postmile')
+    # ax2.set_ylabel('Total flow')
+    # ax2.set_title('SR74 Total Flow BY2019')
+    # plt.show()
